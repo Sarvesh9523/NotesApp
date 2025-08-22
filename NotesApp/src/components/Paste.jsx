b32086eb6dee9e1cb011b0a491694940448dabcf
Sarvesh9523 <sarveshkumar9176@gmail.com>
2025-08-23 00:29:19 +0530

    e

--- NotesApp/src/components/Paste.jsx
+++ NotesApp/src/components/Paste.jsx
@@ -54,68 +54,64 @@ const Paste = () => {
                     </p>
                   </div>
 
-                  {/* icons */}
-                  <div className="flex flex-col gap-y-4 sm:items-end">
-                    <div className="flex gap-2 flex-wrap sm:flex-nowrap">
-                      <button
-                        className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7]  hover:bg-transparent group hover:border-blue-500"
-                        // onClick={() => toast.error("Not working")}
-                      >
-                        <NavLink to={`/?pasteId=${paste?._id}`}>
-                          <PencilLine
-                            className="text-black group-hover:text-blue-500"
-                            size={20}
-                          />
-                        </NavLink>
-                      </button>
-                      <button
-                        className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7]  hover:bg-transparent group hover:border-pink-500"
-                        onClick={() => handleDelete(paste?._id)}
-                      >
-                        <Trash2
-                          className="text-black group-hover:text-pink-500"
-                          size={20}
-                        />
-                      </button>
+                  <div className="flex gap-2 flex-wrap sm:flex-nowrap">
+                    {/* Edit */}
+                    <NavLink
+                      to={`/?pasteId=${paste?._id}`}
+                      className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7] hover:bg-transparent group hover:border-blue-500"
+                    >
+                      <PencilLine
+                        className="text-black group-hover:text-blue-500"
+                        size={20}
+                      />
+                    </NavLink>
 
-                      <button className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7]  hover:bg-transparent group hover:border-orange-500">
-                        <NavLink to={`/pastes/${paste?._id}`} target="_blank">
-                          <Eye
-                            className="text-black group-hover:text-orange-500"
-                            size={20}
-                          />
-                        </NavLink>
-                      </button>
-                      <button
-                        className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7]  hover:bg-transparent group hover:border-green-500"
-                        onClick={() => {
-                          navigator.clipboard.writeText(paste?.content);
-                          toast.success("Copied to Clipboard");
-                        }}
-                      >
-                        <Copy
-                          className="text-black group-hover:text-green-500"
-                          size={20}
-                        />
-                      </button>
-                    </div>
+                    {/* Delete */}
+                    <button
+                      className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7] hover:bg-transparent group hover:border-pink-500"
+                      onClick={() => handleDelete(paste?._id)}
+                    >
+                      <Trash2 className="text-black group-hover:text-pink-500" size={20} />
+                    </button>
 
-                    <div className="gap-x-2 flex ">
-                      <Calendar className="text-black" size={20} />
-                      {FormatDate(paste?.createdAt)}
-                    </div>
+                    {/* View */}
+                    <NavLink
+                      to={`/pastes/${paste?._id}`}
+                      target="_blank"
+                      className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7] hover:bg-transparent group hover:border-orange-500"
+                    >
+                      <Eye className="text-black group-hover:text-orange-500" size={20} />
+                    </NavLink>
+
+                    {/* Copy */}
+                    <button
+                      className="p-2 rounded-[0.2rem] bg-white border border-[#c7c7c7] hover:bg-transparent group hover:border-green-500"
+                      onClick={() => {
+                        navigator.clipboard.writeText(paste?.content);
+                        toast.success("Copied to Clipboard");
+                      }}
+                    >
+                      <Copy className="text-black group-hover:text-green-500" size={20} />
+                    </button>
+                  </div>
+
+
+                  <div className="gap-x-2 flex ">
+                    <Calendar className="text-black" size={20} />
+                    {FormatDate(paste?.createdAt)}
                   </div>
                 </div>
-              ))
-            ) : (
-              <div className="text-2xl text-center w-full text-chileanFire-500">
-                No Data Found
-              </div>
-            )}
+                
+          ))
+          ) : (
+          <div className="text-2xl text-center w-full text-chileanFire-500">
+            No Data Found
           </div>
+            )}
         </div>
       </div>
     </div>
+    </div >
   );
 };
 
